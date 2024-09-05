6b3b307f6c6d86dc9f919f7c2e6149392b148e4f
Alhassan <hassan357020@gmail.com>
2024-09-05 12:15:09 +0100

    completed7

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Weather Journal Web Page
 
-I use use the OpenWeatherMap API as instructed by tutor. Server.js fetches the data from the API and sends it to App.js.
+This project uses the [OpenWeatherMap API](https://openweathermap.org/api) as instructed by the tutor. The `server.js` file handles fetching data from the API and sending it to the frontend (`app.js`).
 
-- Enter a city name and a note and it will display them in a card/log along with the weather info in the city.
-- Previous logs will also be displayed.
-- to delete a log, click on the 'x' on the top right corner of the log.
-- The server does not crash if there is a spelling or network issue.
+### How to Use:
+
+1. Open your browser and enter: `localhost:8000`.
+2. Input a city name and a note. The app will display the entered information along with the weather details for the city in a card/log.
+3. Previous logs will also be displayed on the page.
+4. To delete a log, simply click the 'x' in the top-right corner of the log.
+
+### Error Handling:
+
+- The server handles spelling or network errors gracefully and does not crash in these cases.
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const data = [];
 const cors = require("cors");
 let zipCode = "";
 
-//app.set("viee engine", "ejs");
 app.use(express.json());
 app.use(cors());
 app.use(express.static("demo"));
@@ -15,23 +14,6 @@ app.use(express.static("demo"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
-
-// app.post("/signup", function (req, res) {
-//   id = data.length + 1;
-//   data.push(req.body);
-//   //console.log(data);
-//   req.body[Object.keys(req.body)[0]].id = id;
-//   res.json({ status: "Movie added Successfully" });
-//   console.log(req.body[Object.keys(req.body)[0]]);
-//   //res.sendFile(__dirname + "/views/home.html");
-// });
-// app.get("/signup", (req, res) => {
-//   res.sendFile(__dirname + "/views/signup.html");
-// });
-
-// app.get("/login", (req, res) => {
-//   res.sendFile(__dirname + "/views/login.html");
-// });
 
 const projectData = {};
 const projectData1 = {};
@@ -59,11 +41,7 @@ app.post("/zip", async (req, res) => {
       res.json(projectData1);
     }
   }
-  //  console.log(getData);
   console.log("done");
-
-  //console.log(`${baseUrl}?q=${zipCode}&appid=${apiKey}`);
-  //console.log(zipCode);
 });
 
 const getWeatherData = async (baseUrl, zipCode, apiKey) => {
